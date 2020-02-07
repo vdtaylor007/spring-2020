742efe009b62779d04ebaa5e48ab01fde77ee1ed
vdtaylor007 <vincentdtaylor01@gmail.com>
2020-02-07 03:29:14 +0000

    lab 1.4.1

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
+ //Programmer name: __________
+// Date completed:  __________
+// Description: ___________________________
+
 package main
 
-import "fmt"
+import "fmt" 
 
 func main() {
-  fmt.Println("Hello World")
+    //declare variable for favorite food and store your favorite food.
+    //declare variables for name and age (make sure they are appropriate data types)
+
+    //ask the user to enter their answer for name and age.
+
+    //output a welcome statement using their name
+    //output a statement that says At their age you enjoyed the favorite food
 }
